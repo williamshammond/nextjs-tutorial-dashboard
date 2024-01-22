b324ffed03799b689846e34b9ac10574de69b941
Will Hammond <wsh2117@columbia.edu>
2024-01-21 22:18:02 -0500

    Complete Chapter 16 content of tutorial

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 
+export const metadata = {
+  title: 'Login',
+};
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
